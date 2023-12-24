fe5ac55b43766dc7bff1988447325628da70eed3
prabin <bhandariprabin402@gmail.com>
2023-12-24 21:56:56 +0545

    add user

---
+++ function/user.js
@@ -0,0 +1,10 @@
+const user = {
+  username: "prabin",
+  price: 200,
+};
+function handleobject(anyobject) {
+  console.log(
+    `username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+holderobject(anyobject);
